75e21e0f2c8473825d98833439c0bc9355864134
Mehmet Emre ÖK <144742469+memreok@users.noreply.github.com>
2024-09-22 23:49:51 +0300

    Update MehmetEmreÖk_23010310055_Sort.java

--- MehmetEmreÖk_23010310055_Sort.java
+++ MehmetEmreÖk_23010310055_Sort.java
@@ -64,7 +64,7 @@ public class MehmetEmreÖk_23010310055_Sort {
 		for (int i = 1; i < arr.length; i++) {
 			int key = arr[i];
 			int j = i - 1;
-			while (j >= 0 && arr[i] > key) {
+			while (j >= 0 && arr[j] > key) {
 				arr[j+1] = arr[j];
 				j--;
 			}
